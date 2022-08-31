99d1884f5a8c5dbc6a5ac6763916578b430d0518
Mohammad Arif <danielharold21@gmail.com>
2022-08-31 07:58:25 +0530

    Initial commit

---
+++ app/src/main/java/com/synac/financeappsalesscr/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.synac.financeappsalesscr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.synac.financeappsalesscr.ui.theme.FinanceAppSalesScrTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinanceAppSalesScrTheme {
+
+            }
+        }
+    }
+}
